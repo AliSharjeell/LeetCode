2945f59f5aa99e1319abc9a0c1d1e982f3b0d192
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2024-06-23 10:54:20 +0500

    Time: 69 ms (68.86%) | Memory: 53.6 MB (90.63%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int ans = 0;
+        deque<int> minQueue;
+        deque<int> maxQueue;
+        int s=0,e=0;
+
+        while (e<nums.size())
+        {
+            int x = nums[e];
+            // add end to the minQueue keeping increasing order
+            while (!minQueue.empty()&&nums[minQueue.back()]>=x){
+                minQueue.pop_back();
+            } 
+            minQueue.push_back(e);
+            // add end to the maxQueue keeping decreasing order
+            while (!maxQueue. empty()&&nums[maxQueue. back()] <= x){
+                maxQueue.pop_back();
+            } 
+            maxQueue. push_back(e);
+            int mini = nums[minQueue.front()];
+            int maxi = nums[maxQueue.front()];
+            if (maxi-mini>limit)
+            {
+                s++;
+                if (s>minQueue.front()){
+                    minQueue.pop_front();
+                } 
+                if (s>maxQueue.front()){
+                    maxQueue.pop_front();
+                }
+            } 
+            else
+            {
+                ans=max(ans,e-s+1);
+                e++;
+            }
+        }
+            return ans; 
+    }
+};
+
+
