145616bce71f8bca4c75cf87acec5e5476484eef
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2024-06-30 11:01:08 +0500

    Time: 487 ms (23.94%) | Memory: 159.2 MB (36.70%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,10 +1,10 @@
 class DSU {
     vector<int> parent;
-    vector<int> rank;
+    // vector<int> rank;
 public:
     DSU(int n) {
         parent.resize(n);
-        rank.resize(n, 0);
+        // rank.resize(n, 0);
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
@@ -19,14 +19,8 @@ public:
         int rootY = Find(y);
         
         if (rootX != rootY) {
-            // if (rank[rootX] > rank[rootY]) {
-            //     parent[rootY] = rootX;
-            // } else if (rank[rootX] < rank[rootY]) {
-            //     parent[rootX] = rootY;
-            // } else {
                 parent[rootY] = rootX;
-                rank[rootX]++;
-            // }
+                // rank[rootX]++;
                 return true;
         }
         return false;
