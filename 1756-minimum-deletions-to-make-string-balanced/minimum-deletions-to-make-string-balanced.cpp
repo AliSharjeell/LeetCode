5e08d757ffbf7feef4471cca684e80f4e8b0b2d2
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2024-07-30 21:09:31 +0500

    Time: 104 ms (40.03%) | Memory: 38.4 MB (37.32%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int acnt = 0, bcnt = 0;
+        int acnt = 0,bcnt = 0;
         vector<int> barr(s.length());
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] == 'b') {
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='b'){
                 bcnt++;
             }
-            barr[i] = bcnt;
+            barr[i]=bcnt;
         }
-
         int mini = INT_MAX;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == 'a') {
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a'){
                 acnt++;
             }
-            mini = min(mini, (int)s.length() - (acnt + barr[i]));
+            mini = min(mini,(int)(s.length()-(acnt+barr[i])));
         }
-
         return mini;
     }
 };
