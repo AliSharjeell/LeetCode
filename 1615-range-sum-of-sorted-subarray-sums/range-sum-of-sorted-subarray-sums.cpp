7d637ddc2f35813025cc2ce6e0f9bb28251f616b
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2024-08-04 06:54:44 +0500

    Time: 95 ms (42.70%) | Memory: 27.2 MB (31.24%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        int mod=1e9+7;
         vector<int> arr;
         for(int i=0;i<n;i++){
             int sum=0;
             for(int j=i;j<n;j++){
                 sum+=nums[j];
-                // cout<<sum<<" ";
                 arr.push_back(sum);
             }
         }
@@ -16,6 +14,7 @@ public:
         for(int i=left-1;i<=right-1;i++){
             ans+=arr[i];
         }
+        int mod=1e9+7;
         return ans%=mod;
     }
 };
