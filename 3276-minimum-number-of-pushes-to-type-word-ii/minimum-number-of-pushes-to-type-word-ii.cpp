c531625bbbf46b4329c072626b5b929b62e4fd87
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2024-08-06 20:49:18 +0500

    Time: 60 ms (95.89%) | Memory: 20.9 MB (98.11%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void print(auto& c){
+        for(int x: c) cout<<x<<", "; 
+        cout<<endl;
+    }
+    int minimumPushes(string& word) {
+        int freq[26]={0};
+    //    memset(freq, 0, sizeof(freq));
+        for(char c: word) 
+            freq[c-'a']++;
+        sort(freq, freq+26, greater<int>());
+    //    print(freq);
+        int sz=0, push=1, ans=0;
+        for(; sz<26 && freq[sz]!=0; sz++){
+            if (sz>=8 && sz%8==0) push++;
+            ans+=freq[sz]*push;    
+        }
+        return ans;
+    }
+};
