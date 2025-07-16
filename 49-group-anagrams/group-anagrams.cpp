541871b1f109ade4a820cd7db2d3c2c6a5ee66f0
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-16 22:21:29 +0500

    Time: 1163 ms (5.02%) | Memory: 32 MB (14.91%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       vector<pair<unordered_map<char,int>,string>> storage(strs.size());
+       for(int i=0;i<strs.size();i++){
+            storage[i].second = strs[i];
+            for(int j=0;j<strs[i].size();j++){
+                storage[i].first[strs[i][j]]++;
+            }
+       }
+       vector<vector<string>> answer;
+       int index=0;
+        vector<bool> used(strs.size(),false);
+        for(int i=0;i<strs.size();i++){
+        index++;
+        vector<string> answergroup;
+        if(used[i]==false){
+         answergroup.push_back(storage[i].second);
+        }
+            for(int j=0;j<strs.size();j++){
+                if(storage[i].first==storage[j].first&&i!=j && used[j]==false){
+                    answergroup.push_back(storage[j].second);
+                    used[j]=true;
+                }
+            }
+            if(!answergroup.empty())
+            {
+                answer.push_back(answergroup);
+            }
+            used[i]=true;
+        }
+        return answer;
+    }
+};
