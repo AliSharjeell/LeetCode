3c01824c77e8b5633cbc0f50004e2258ffca27eb
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-16 23:28:42 +0500

    Time: 4 ms (35.32%) | Memory: 17.8 MB (76.61%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,16 +5,26 @@ public:
         for(auto i:nums){
             map[i]++;
         }
-        vector<pair<int,int>> answer;
+        // vector<pair<int,int>> answer;
+        // for(auto i:map){
+        //     answer.push_back({i.second,i.first});
+        // }
+        // sort(answer.begin(),answer.end());
+        // reverse(answer.begin(),answer.end());
+        // answer.erase(answer.begin()+k,answer.end());
+        // vector<int> final;
+        // for(int i=0;i<answer.size();i++){
+        //     final.push_back(answer[i].second);
+        // }
+        // return final;
+        priority_queue<pair<int,int>> pq;
         for(auto i:map){
-            answer.push_back({i.second,i.first});
+            pq.push({i.second,i.first});
         }
-        sort(answer.begin(),answer.end());
-        reverse(answer.begin(),answer.end());
-        answer.erase(answer.begin()+k,answer.end());
         vector<int> final;
-        for(int i=0;i<answer.size();i++){
-            final.push_back(answer[i].second);
+        for(int i=0;i<k;i++){
+            final.push_back(pq.top().second);
+            pq.pop();
         }
         return final;
     }
