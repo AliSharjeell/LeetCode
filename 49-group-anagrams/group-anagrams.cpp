9e2b250ca89ba1e5e46d421481474befa90d7ce5
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-16 22:56:11 +0500

    Time: 44 ms (17.64%) | Memory: 36 MB (7.23%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,32 +1,90 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       vector<pair<unordered_map<char,int>,string>> storage(strs.size());
-       for(int i=0;i<strs.size();i++){
-            storage[i].second = strs[i];
-            for(int j=0;j<strs[i].size();j++){
-                storage[i].first[strs[i][j]]++;
-            }
-       }
-       vector<vector<string>> answer;
-        vector<bool> used(strs.size(),false);
+        unordered_map<string,vector<string>> prevans;
         for(int i=0;i<strs.size();i++){
-        vector<string> answergroup;
-        if(used[i]==false){
-         answergroup.push_back(storage[i].second);
+            map<char,int> storage;
+            string uniquekey = "";
+            for(int j=0;j<strs[i].size();j++){
+                storage[strs[i][j]]++;
+            }
+            for(auto i:storage){
+                uniquekey+=i.first;
+                uniquekey+=to_string(i.second);
+            }
+            prevans[uniquekey].push_back(strs[i]);
         }
-            for(int j=0;j<strs.size();j++){
-                if(storage[i].first==storage[j].first&&i!=j && used[j]==false){
-                    answergroup.push_back(storage[j].second);
-                    used[j]=true;
-                }
-            }
-            if(!answergroup.empty())
-            {
-                answer.push_back(answergroup);
-            }
-            used[i]=true;
+        vector<vector<string>> answer;
+        for(auto i:prevans){
+            answer.push_back(i.second);
         }
         return answer;
+        
+        
+        
+        
+        // unordered_map<int,vector<string>> prevans;
+        // for(int i=0;i<strs.size();i++){
+        //     int uniquekey=0;
+        //     for(int j=0;j<strs[i].size();j++){
+        //         uniquekey+= strs[i][j];
+        //     }
+        //     prevans[uniquekey].push_back(strs[i]);
+        // }
+        // vector<vector<string>> answer;
+        // for(auto i:prevans){
+        //     answer.push_back(i.second);
+        // }
+        // return answer;
+
+
+
+
+        
+    //    vector<pair<unordered_map<char,int>,string>> storage(strs.size());
+    //    for(int i=0;i<strs.size();i++){
+    //         storage[i].second = strs[i];
+    //         for(int j=0;j<strs[i].size();j++){
+    //             storage[i].first[strs[i][j]]++;
+    //         }
+    //    }
+    //    unordered_map<unordered_map<char,int>,vector<string>> answerb4;
+    //    for(int i=0;i<strs.size();i++){
+    //         answerb4[storage[i].first].push_back(storage[i].second);
+    //    }
+    //    vector<vector<string>> answer;
+    //    for(auto i:answerb4){
+    //     answer.push_back(i.second);
+    //    }
+    //    return answer;
+
+
+
+
+
+
+
+
+
+    //    vector<vector<string>> answer;
+    //     vector<bool> used(strs.size(),false);
+    //     for(int i=0;i<strs.size();i++){
+    //     vector<string> answergroup;
+    //     if(used[i]==false){
+    //      answergroup.push_back(storage[i].second);
+    //     }
+    //         for(int j=0;j<strs.size();j++){
+    //             if(storage[i].first==storage[j].first&&i!=j && used[j]==false){
+    //                 answergroup.push_back(storage[j].second);
+    //                 used[j]=true;
+    //             }
+    //         }
+    //         if(!answergroup.empty())
+    //         {
+    //             answer.push_back(answergroup);
+    //         }
+    //         used[i]=true;
+    //     }
+    //     return answer;
     }
 };
