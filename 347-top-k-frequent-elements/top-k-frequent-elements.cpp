fc64bba46532b46d5f10ed6d073a47016219b029
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-22 04:14:56 +0500

    Time: 1 ms (65.72%) | Memory: 20 MB (7.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,6 +5,49 @@ public:
         for(auto i:nums){
             map[i]++;
         }
+        vector<vector<int>> storage(nums.size());
+        for(auto [i,j]:map){
+            storage[j-1].push_back(i);
+        }
+        vector<int> answer;
+        int cnt=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(!storage[i].empty()){
+                for(auto j:storage[i]){
+                    answer.push_back(j);
+                    cnt++;
+                    if(cnt>=k){
+                        break;
+                    }
+                }
+                if(cnt>=k){
+                        break;
+                    }
+            }
+            if(cnt>=k){
+                        break;
+                    }
+        }
+        return answer;
+
+        // unordered_map<int,int> map;
+        // for(auto i:nums){
+        //     map[i]++;
+        // }
+        // priority_queue<pair<int,int>> pq;
+        // for(auto i:map){
+        //     pq.push({i.second,i.first});
+        // }
+        // vector<int> final;
+        // for(int i=0;i<k;i++){
+        //     final.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return final;
+
+
+
+
         // vector<pair<int,int>> answer;
         // for(auto i:map){
         //     answer.push_back({i.second,i.first});
@@ -17,15 +60,5 @@ public:
         //     final.push_back(answer[i].second);
         // }
         // return final;
-        priority_queue<pair<int,int>> pq;
-        for(auto i:map){
-            pq.push({i.second,i.first});
-        }
-        vector<int> final;
-        for(int i=0;i<k;i++){
-            final.push_back(pq.top().second);
-            pq.pop();
-        }
-        return final;
     }
 };
