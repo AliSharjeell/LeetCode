63eb6ad9f41b6ce9af0822cf9806051ec48d1490
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-22 04:47:13 +0500

    Time: 0 ms (100.00%) | Memory: 40.1 MB (78.97%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> pre(nums.size(),1);
+        vector<int> ans(nums.size(),1);
         for(int i=1;i<nums.size();i++){
-            pre[i]=nums[i-1]*pre[i-1];
+            ans[i]=nums[i-1]*ans[i-1];
         }
-        vector<int> post(nums.size(),1);
-        for(int i=nums.size()-2;i>=0;i--){
-            post[i]=nums[i+1]*post[i+1];
+        int post = 1;
+        for(int i=nums.size()-1;i>=0;i--){
+            ans[i]*=post;
+            post*=nums[i];
         }
-        // for(auto i:)
-        vector<int> answer(nums.size());
-        for(int i=0;i<nums.size();i++){
-            answer[i]=pre[i]*post[i];
-        }
-        return answer;
+        return ans;
+
+
+
+        // vector<int> pre(nums.size(),1);
+        // for(int i=1;i<nums.size();i++){
+        //     pre[i]=nums[i-1]*pre[i-1];
+        // }
+        // vector<int> post(nums.size(),1);
+        // for(int i=nums.size()-2;i>=0;i--){
+        //     post[i]=nums[i+1]*post[i+1];
+        // }
+        // vector<int> answer(nums.size());
+        // for(int i=0;i<nums.size();i++){
+        //     answer[i]=pre[i]*post[i];
+        // }
+        // return answer;
     }
 };
