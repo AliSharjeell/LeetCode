a6bd97ba46632261f7f1674e0fc907ff512969d6
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-23 02:54:55 +0500

    Time: 83 ms (57.45%) | Memory: 88.9 MB (49.35%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set store(nums.begin(),nums.end());
+        int currSeqLength = 0;
+        int maxSeqLength = 0;
+        for(auto i:store){
+            if(!store.count(i-1)){
+                int j=i;
+                while(store.count(j)){
+                    currSeqLength++;
+                    j++;
+                }
+                maxSeqLength = max(maxSeqLength,currSeqLength);
+                currSeqLength=0;
+            }
+        }
+        return maxSeqLength;
+
+
+
+        // long long size = 500;
+        // vector<int> store(size,0);
+        // for(int i:nums){
+        //     store[i]=1;
+        // }
+        // bool subIsOn = false;
+        // int currSeqLength = 0;
+        // int maxSeqLength = 0;
+        // for(int i:nums){
+        //     if(i==1){
+        //         subIsOn=true;
+        //     }
+        //     if(subIsOn){
+        //         currSeqLength++;
+        //     }
+        //     if(i==0){
+        //         subIsOn=false;
+        //         maxSeqLength = max(maxSeqLength,currSeqLength);
+        //         currSeqLength=0;
+        //     }
+        // }
+        // return maxSeqLength;
+    }
+};
