091766191a0f4bf3ba5735074079a8e1aa267ffb
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-07-23 05:11:13 +0500

    Time: 3 ms (31.91%) | Memory: 9.8 MB (94.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,15 +4,31 @@ public:
         for(char &i:s){
             i=tolower(i);
         }
-        for(int i=0;i<s.length();i++){
-            if(!(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9')){
-                s.erase(s.begin()+i);
-                i--;
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            while(i<j&&!(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9')){
+                i++;
             }
+             while(i<j&&!(s[j]>='a'&&s[j]<='z'||s[j]>='0'&&s[j]<='9')){
+                j--;
+            }
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++,j--;
         }
-        cout<<s;
-        string str = s;
-        reverse(str.begin(), str.end());
-        return s == str;
+            return true;
+        // string ans="";
+        // for(char &i:s){
+        //     i=tolower(i);
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]>='a'&&s[i]<='z'||s[i]>='0'&&s[i]<='9'){
+        //         ans+=s[i];
+        //     }
+        // }
+        // cout<<ans;
+        // return ans == string(ans.rbegin(),ans.rend());
     }
 };
