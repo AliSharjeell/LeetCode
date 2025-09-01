3e4b0fcad9471fc937daa01779a7671348b0fb88
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-09-01 20:07:56 +0500

    Time: 479 ms (5.06%) | Memory: 18.5 MB (66.32%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode dummy(0);
+        ListNode* dummyptr = &dummy;
+        vector<ListNode*> temp(lists.size());
+        for(int i=0;i<lists.size();i++){
+            temp[i] = lists[i];
+        }
+        while(true){
+            int cnt=0;
+            for(int i=0;i<lists.size();i++){
+                if(!temp[i]){
+                    cnt++;
+                }
+            }
+            if(cnt==lists.size()){
+                break;
+            }
+            int min=INT_MAX;
+            int mini=-1;
+            for(int i=0;i<lists.size();i++){
+                if(temp[i]&&temp[i]->val<min){
+                    min=temp[i]->val;
+                    mini=i;
+                }
+            }
+                    cout<<min<<" ";
+            dummyptr->next=temp[mini];
+            dummyptr=dummyptr->next;
+            temp[mini]=temp[mini]->next;
+        }
+        return dummy.next;
+    }
+};
