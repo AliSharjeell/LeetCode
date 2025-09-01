66ad3193dd4d86a223f5ef7f237700e2c7d233a3
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-09-01 20:36:35 +0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (81.94%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,37 +8,57 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+    struct compare{
+        bool operator()(ListNode* a,ListNode* b){
+            return a->val > b->val;
+        }
+    };  
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // ListNode dummy(0);
+        // ListNode* dummyptr = &dummy;
+        // while(true){
+        //     int cnt=0;
+        //     for(int i=0;i<lists.size();i++){
+        //         if(!lists[i]){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt==lists.size()){
+        //         break;
+        //     }
+        //     int min=INT_MAX;
+        //     int mini=-1;
+        //     for(int i=0;i<lists.size();i++){
+        //         if(lists[i]&&lists[i]->val<min){
+        //             min=lists[i]->val;
+        //             mini=i;
+        //         }
+        //     }
+        //             cout<<min<<" ";
+        //     dummyptr->next=lists[mini];
+        //     dummyptr=dummyptr->next;
+        //     lists[mini]=lists[mini]->next;
+        // }
+        // return dummy.next;
+
         ListNode dummy(0);
         ListNode* dummyptr = &dummy;
-        // vector<ListNode*> temp(lists.size());
-        // for(int i=0;i<lists.size();i++){
-        //     temp[i] = lists[i];
-        // }
-        while(true){
-            int cnt=0;
-            for(int i=0;i<lists.size();i++){
-                if(!lists[i]){
-                    cnt++;
-                }
-            }
-            if(cnt==lists.size()){
-                break;
-            }
-            int min=INT_MAX;
-            int mini=-1;
-            for(int i=0;i<lists.size();i++){
-                if(lists[i]&&lists[i]->val<min){
-                    min=lists[i]->val;
-                    mini=i;
-                }
-            }
-                    cout<<min<<" ";
-            dummyptr->next=lists[mini];
+        priority_queue<ListNode*,vector<ListNode*>,compare> minHeap;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i])
+            minHeap.push(lists[i]);
+        }
+        while(!minHeap.empty()){
+            dummyptr->next=minHeap.top();
             dummyptr=dummyptr->next;
-            lists[mini]=lists[mini]->next;
+            ListNode* temp = minHeap.top();
+            minHeap.pop();
+            if(temp->next){
+                temp=temp->next;
+                minHeap.push(temp);
+            }
         }
         return dummy.next;
     }
