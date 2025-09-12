ad23c8d74658e7028803ce032fea1bb685482bff
Ali Sharjeel <141981610+AliSharjeell@users.noreply.github.com>
2025-09-12 23:15:11 +0500

    Time: 26 ms (26.13%) | Memory: 23.5 MB (64.80%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,22 +9,34 @@
  */
 
 class Solution {
-    void dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode* &ans){
-        if(p->val < root->val && q->val < root->val){
-            dfs(root->left, p, q,ans);
-        }
-        else if(p->val > root->val && q->val > root->val){
-            dfs(root->right, p, q,ans);
-        }
-        else{
-            ans = root;
-            return;
-        }
-    }
+    // void dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode* &ans){
+    //     if(p->val < root->val && q->val < root->val){
+    //         dfs(root->left, p, q,ans);
+    //     }
+    //     else if(p->val > root->val && q->val > root->val){
+    //         dfs(root->right, p, q,ans);
+    //     }
+    //     else{
+    //         ans = root;
+    //         return;
+    //     }
+    // }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* ans = nullptr;
-        dfs(root, p, q, ans);
-        return ans;
+        // TreeNode* ans = nullptr;
+        // dfs(root, p, q, ans);
+        // return ans;
+        while(root){
+            if(p->val < root->val && q->val < root->val){
+                root=root->left;
+            }
+            else if(p->val > root->val && q->val > root->val){
+                 root=root->right;
+            }
+            else{
+                return root;
+            }
+        }
+        return nullptr;
     }
 };
